50ecc0390865225f09d01c78fc3704279585e5f2
Swapnill0 <rajput.swapnil033@gmail.com>
2026-06-04 00:50:18 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,55 @@
+//require('dotenv').config({path:'./env'})
+import dotenv from "dotenv";
+import dns from "node:dns"; // Or const dns = require("node:dns");
+dns.setServers(["0.0.0.0", "8.8.8.8","1.1.1.1"]);
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: './env'
+})
+
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+( async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("error",error);
+            throw error
+        })
+    
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listning on port ${process.env.PORT}`);
+        })
+
+    }
+    catch(error){
+        console.error("ERROR",error)
+        throw err
+    }
+})()*/
